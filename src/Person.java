0123bd20843d9efae5e9667aee01a3be417e2004
Rajathbharadwaj <rajathrajath940@gmail.com>
2017-05-18 10:25:43 +0530

    Added Person class

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+/**
+ * Created by BIC on 5/18/2017.
+ */
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
