cc1ec85f80d67f4172439de63b6a49ab722edcc4
Rajathbharadwaj <rajathrajath940@gmail.com>
2017-05-18 10:20:21 +0530

    This is where I started off

---
+++ src/learning.java
@@ -0,0 +1,14 @@
+/**
+ * Created by BIC on 5/13/2017.
+ */
+public class learning {
+
+    public static void main(String[] args) {
+
+       System.out.println("To convert 200 miles into Km");
+
+     double kilometers = (200 * 1.609344);
+        System.out.println("200 mi in Km = " + kilometers);
+
+    }
+}
