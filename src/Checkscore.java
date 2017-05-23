cc8383ca9393fb98939dcbd4c066ae9b18336e23
Rajathbharadwaj <rajathrajath940@gmail.com>
2017-05-23 12:52:41 +0530

    Add files via upload

---
+++ src/Checkscore.java
@@ -0,0 +1,35 @@
+/**
+ * Created by BIC on 5/23/2017.
+ */
+public class Checkscore {
+
+    public static void main(String[] args) {
+
+//        boolean gameOver = true;
+//        int score = 800;
+//        int levelCompleted = 5;
+//        int bonus = 100;
+//
+//
+//        if(gameOver == true) {
+//
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was" + finalScore);
+//        }
+
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+
+        if(gameOver == true) {
+
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+    }
+
+}
