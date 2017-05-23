272e00a0fe2deddd2564267a72c9763ebb716ca0
Rajathbharadwaj <rajathrajath940@gmail.com>
2017-05-23 20:11:47 +0530

    Add files via upload

---
+++ src/Foreal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+ /*Please dont change class name, Dcoder 
+ and class must not be public*/
+
+ //Compiler version JDK 1.8
+
+
+ class Dcoder
+ { 
+	public static void main(String args[])
+ 	{ 
+
+ int Score = 56;
+ boolean realScore = true;
+ int type = 44;
+ 
+ if(realScore){
+ int foreal = 100 + (Score * type);
+ System.out.println("This is foreal " +foreal);
+ }
+ 
+
+ 	}
+ }
