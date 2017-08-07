d4aefe46400aba692973c3b1cf5bc73940b3f89a
Rajath Bharadwaj <rajathrajath940@gmail.com>
2017-08-07 22:18:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,18 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        class ClickListener implements Button.OnClickListener {
-//            public ClickListener() {
-//                System.out.println("I've been attached");
-//            }
-//
-//            @Override
-//            public void onClick(String title) {
-//                System.out.println(title + " was clicked");
-//            }
-//        }
-//
-//        btnPrint.setOnClickListener(new ClickListener());
+
         btnPrint.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(String title) {
